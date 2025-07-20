5945c3bbab7b6e59d9cf22439b2342bde174e706
Josh Koter <josh.koter@gmail.com>
2025-07-20 12:20:07 +0200

    ...

---
+++ README.md
@@ -0,0 +1,5 @@
+# Making games with Ruby (and other bad ideas)
+
+## Resources
+
+1. https://www.ruby2d.com/
---
+++ triangle.rb
@@ -0,0 +1,12 @@
+require 'ruby2d'
+
+set title: "Sup Tri'"
+
+Triangle.new(
+	x1: 320, y1: 50,
+	x2: 540, y2: 430,
+	x3: 100, y3: 430,
+	color: ['green', 'red', 'blue']
+)
+
+show
