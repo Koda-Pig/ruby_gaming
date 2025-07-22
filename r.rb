feb81dcf430c7729acc6f8fbc84de03bf75e4390
Josh Koter <josh.koter@gmail.com>
2025-07-22 21:56:03 +0200

    ...

--- r.rb
+++ r.rb
@@ -7,6 +7,8 @@ canvas_height = 506
 sprite_width = 200
 sprite_height = 182
 last_direction = 'right'
+velocity_y = 0
+weight = 1
 
 set width: canvas_width
 set height: canvas_height
@@ -16,10 +18,10 @@ set height: canvas_height
 # 16800 / 200 = 84 columns
 # All animations in this sprite sheet point right
 # use the 'flip' property for left pointing animations
-sprite = Sprite.new(
+@sprite = Sprite.new(
 	'dog_sprite_horiz.png',
 	x: canvas_width / 2 - sprite_width / 2,
-	y: canvas_height / 2 - sprite_height / 2,
+	y: sprite_height * -1, # make him drop from the top for fun
 	width: sprite_width,
 	height: sprite_height,
 	clip_width: sprite_width,
@@ -34,6 +36,7 @@ sprite = Sprite.new(
 		roll: 35..41,
 	}
 )
+
 
 # initial player state
 player_state = 'standing_right'
@@ -56,12 +59,13 @@ on :key_down do |event|
 end
 
 on :key_up do |event|
-	sprite.stop
+	@sprite.stop
 	pressed_keys.delete(event.key)
 end
 
 
 update do
+	# set player state according to user input
 	if pressed_keys.include?('up')
 		player_state = "jumping_#{last_direction}"
 	elsif pressed_keys.include?('space')
@@ -76,27 +80,44 @@ update do
 		player_state = "standing_#{last_direction}"
 	end
 
+	@sprite.y += velocity_y
+
+	is_on_ground = @sprite.y >= canvas_height - @sprite.height
+
+	if is_on_ground
+		velocity_y = 0
+	else
+		velocity_y += weight
+	end
+
+	# prevent player falling through floor
+	if @sprite.y > canvas_height - @sprite.height
+		@sprite.y = canvas_height - @sprite.height
+	end
+
+
+	# handle each state
 	case player_state
 	when 'standing_right'
-		sprite.play(animation: :stand, loop: true)
+		@sprite.play(animation: :stand, loop: true)
 	when 'standing_left'
-		sprite.play(animation: :stand, loop: true, flip: :horizontal)
+		@sprite.play(animation: :stand, loop: true, flip: :horizontal)
 	when 'running_right'
-		sprite.play(animation: :run, loop: true)
+		@sprite.play(animation: :run, loop: true)
 	when 'running_left'
-		sprite.play(animation: :run, loop: true, flip: :horizontal)
+		@sprite.play(animation: :run, loop: true, flip: :horizontal)
 	when 'jumping_right'
-		sprite.play(animation: :jump, loop: true)
+		@sprite.play(animation: :jump, loop: true)
 	when 'jumping_left'
-		sprite.play(animation: :jump, loop: true, flip: :horizontal)
+		@sprite.play(animation: :jump, loop: true, flip: :horizontal)
 	when 'sitting_right'
-		sprite.play(animation: :sit, loop: true)
+		@sprite.play(animation: :sit, loop: true)
 	when 'sitting_left'
-		sprite.play(animation: :sit, loop: true, flip: :horizontal)
+		@sprite.play(animation: :sit, loop: true, flip: :horizontal)
 	when 'rolling_right'
-		sprite.play(animation: :roll, loop: true)
+		@sprite.play(animation: :roll, loop: true)
 	when 'rolling_left'
-		sprite.play(animation: :roll, loop: true, flip: :horizontal)
+		@sprite.play(animation: :roll, loop: true, flip: :horizontal)
 	end
 end
 
