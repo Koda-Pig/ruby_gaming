ee80b448742e83a6a6311c14a7581c64e820e523
Josh Koter <josh.koter@gmail.com>
2025-07-26 12:01:55 +0200

    ...

--- r.rb
+++ r.rb
@@ -14,7 +14,7 @@ set height: $GAME_HEIGHT
 # 16800 / 200 = 84 columns
 # All animations in this sprite sheet point right
 # use the 'flip' property for left pointing animations
-player = Sprite.new(
+player_sprite = Sprite.new(
 	'dog_sprite_horiz.png',
 	x: $GAME_WIDTH / 2 - PLAYER_WIDTH / 2,
 	y: PLAYER_HEIGHT * -1, # make him drop from the top for fun
@@ -60,13 +60,13 @@ on :key_down do |event|
 end
 
 on :key_up do |event|
-	@player.stop
+	@state.sprite.stop
 	@state.pressed_keys.delete(event.key)
 end
 
 # animation loop
 update do
-	is_on_ground = @player.y >= $GAME_HEIGHT - @player.height
+	is_on_ground = @state.sprite.y >= $GAME_HEIGHT - @state.sprite.height
 
 	# Roll timer logic
 	@attack_timer.update
@@ -103,7 +103,7 @@ update do
 	end
 
 	# player position
-	@player.y += @state.velocity_y
+	@state.sprite.y += @state.velocity_y
 
 	if is_on_ground
 		@state.velocity_y = 0
@@ -112,24 +112,24 @@ update do
 	end
 
 	# prevent player falling through floor
-	if @player.y > $GAME_HEIGHT - @player.height
-		@player.y = $GAME_HEIGHT - @player.height
+	if @state.sprite.y > $GAME_HEIGHT - @state.sprite.height
+		@state.sprite.y = $GAME_HEIGHT - @state.sprite.height
 	end
 
 	# handle each state
 	case @state.action
 	when 'standing_right'
-		@player.play(animation: :stand, loop: true)
+		@state.sprite.play(animation: :stand, loop: true)
 	when 'standing_left'
-		@player.play(animation: :stand, loop: true, flip: :horizontal)
+		@state.sprite.play(animation: :stand, loop: true, flip: :horizontal)
 	when 'running_right'
-		@player.play(animation: :run, loop: true)
+		@state.sprite.play(animation: :run, loop: true)
 		update_background('right')
 	when 'running_left'
-		@player.play(animation: :run, loop: true, flip: :horizontal)
+		@state.sprite.play(animation: :run, loop: true, flip: :horizontal)
 		update_background('left')
 	when 'jumping_right'
-		@player.play(animation: :jump, loop: true)
+		@state.sprite.play(animation: :jump, loop: true)
 		if is_on_ground
 			@state.velocity_y -= 20
 		end
@@ -138,7 +138,7 @@ update do
 			update_background('right')
 		end
 	when 'jumping_left'
-		@player.play(animation: :jump, loop: true, flip: :horizontal)
+		@state.sprite.play(animation: :jump, loop: true, flip: :horizontal)
 		if is_on_ground
 			@state.velocity_y -= 20
 		end
@@ -147,23 +147,23 @@ update do
 			update_background('left')
 		end
 	when 'sitting_right'
-		@player.play(animation: :sit, loop: true)
+		@state.sprite.play(animation: :sit, loop: true)
 	when 'sitting_left'
-		@player.play(animation: :sit, loop: true, flip: :horizontal)
+		@state.sprite.play(animation: :sit, loop: true, flip: :horizontal)
 	when 'attacking_right'
-		@player.play(animation: :attack, loop: true)
+		@state.sprite.play(animation: :attack, loop: true)
 		update_background('right', ATTACK_ACCELERATION)
 	when 'attacking_left'
-		@player.play(animation: :attack, loop: true, flip: :horizontal)
+		@state.sprite.play(animation: :attack, loop: true, flip: :horizontal)
 		update_background('left', ATTACK_ACCELERATION)
 	when 'falling_right'
-		@player.play(animation: :fall, loop: true)
+		@state.sprite.play(animation: :fall, loop: true)
 		# Only move when user is also holding directional key
 		if @state.pressed_keys.include?('right')
 			update_background('right')
 		end
 	when 'falling_left'
-		@player.play(animation: :fall, loop: true, flip: :horizontal)
+		@state.sprite.play(animation: :fall, loop: true, flip: :horizontal)
 		# Only move when user is also holding directional key
 		if @state.pressed_keys.include?('left')
 			update_background('left')
--- state.rb
+++ state.rb
@@ -11,35 +11,11 @@ class PlayerState
 
   def initialize(initial_action, player_sprite)
 		@action = initial_action
+		@sprite = player_sprite
 		@pressed_keys = Set.new
 		@can_attack = true
 		@last_direction = 'right'
 		@velocity_y = 0
-
-		# Image dimensions: 8400 × 182
-		# Sprite frame dimensions: 200x182
-		# 16800 / 200 = 84 columns
-		# All animations in this sprite sheet point right
-		# use the 'flip' property for left pointing animations
-		@player = Sprite.new(
-			'dog_sprite_horiz.png',
-			x: $GAME_WIDTH / 2 - PLAYER_WIDTH / 2,
-			y: PLAYER_HEIGHT * -1, # make him drop from the top for fun
-			width: PLAYER_WIDTH,
-			height: PLAYER_HEIGHT,
-			clip_width: PLAYER_WIDTH,
-			clip_height: PLAYER_HEIGHT,
-			time: 60,
-			animations: {
-				stand: 0..6,
-				jump: 7..13,
-				fall: 14..20,
-				run: 21..29,
-				sit: 30..34,
-				attack: 35..41,
-			}
-		)
-
   end
 
 	def fall(direction)
